ec66ad5ab771e4494f4cab8350dfd17a6df8c52a
GeraldYang <578634482@qq.com>
2017-11-07 00:03:21 +0800

    django-init

---
+++ PycharmProjects/auto_reply/polls/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You are at the polls index.")
+
